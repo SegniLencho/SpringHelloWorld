166932c55c4ca5f86dfc0b7063cf81e4f0969d9e
SegniLencho <segnitemesgenlen@gmail.com>
2017-06-14 18:38:32 -0600

    Initial commit

---
+++ src/main/java/cs544/com/segnipack/Home.java
@@ -0,0 +1,42 @@
+package cs544.com.segnipack;
+
+/**
+ * Created by OD on 6/14/2017.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class Home {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+        return "hello";
+
+    }
+    @RequestMapping(value = "/segni")
+    public String segni(ModelMap model){
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+        return "segni";
+    }
+
+    @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("hello");
+        model.addObject("msg", name);
+
+        return model;
+
+    }
+
+}
